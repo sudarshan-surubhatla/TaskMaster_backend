a33dbf5430dc3e66073e2b82967a9aa218301f34
Sudarshan Kumar Surubhatla <143895204+sudarshan-surubhatla@users.noreply.github.com>
2023-11-21 21:56:53 +0530

    Update taskController.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,11 +1,14 @@
 import taskModel from "../models/taskModel.js";
 import userModel from "../models/userModel.js";
 import { createTransport } from 'nodemailer';
+import moment from 'moment-timezone';
 import dotenv from "dotenv";
-import cron from "node-cron";
+import cron from 'node-cron';
 
-const sendMail = (email, subject, title, description, isReminder = false, isDeleted = false) => {
-    console.log("Sending email to ", email);
+dotenv.config();
+
+// Function to send email
+const sendMail = (email, subject, title, description, isReminder, isDeleted) => {
     var transporter = createTransport({
         service: 'gmail',
         auth: {
@@ -20,8 +23,8 @@ const sendMail = (email, subject, title, description, isReminder = false, isDele
         subject: subject,
         html: `
             <div style="font-family: 'Roboto', sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
-            <h1 style="color:  '#007BFF'; text-align: center; margin-bottom: 20px;">
-                    ${subject}
+            <h1 style="color: ${isDeleted ? '#FF5555' : '#007BFF'}; text-align: center; margin-bottom: 20px;">
+                    ${isDeleted ? 'Task Deleted' : isReminder ? 'Task Due Soon' : 'Task Added Successfully'}
                 </h1>
                 <div style="background-color: #ffffff; border: 1px solid #ddd; border-radius: 8px; padding: 20px; margin-bottom: 20px;">
                     <h2 style="color: #333; margin-bottom: 10px;">Title: ${title}</h2>
@@ -29,7 +32,7 @@ const sendMail = (email, subject, title, description, isReminder = false, isDele
                 </div>
                 <p style="color: #777; font-size: 14px; text-align: center;">Thank you for using TaskMaster!</p>
             </div>`,
-    };
+    }
 
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
@@ -40,48 +43,60 @@ const sendMail = (email, subject, title, description, isReminder = false, isDele
     });
 }
 
-const checkDueTasks = async () => {
-    const approachingDueTasks = await taskModel.find({
-        datetime: {
-            $gte: new Date(),
-            $lte: new Date(Date.now() + 10 * 60 * 1000)
+// Function to schedule email for task reminder
+const scheduleEmail = (task) => {
+    console.log("Scheduling email for task...");
+    const reminderTime = moment(task.datetime).subtract(5, 'hours').subtract(30, 'minutes');
+    console.log('Reminder email will be sent at:', reminderTime.format('mm HH DD MM ddd'));
+    const job = cron.schedule(
+        reminderTime.format('mm HH DD MM ddd'),
+        async function () {
+            const { email, title, description } = task;
+            try {
+                console.log('Schedule function called');
+                sendMail(email, "Task Due Soon", title, description, true, false);
+                console.log('Reminder Email sent successfully');
+            } catch (error) {
+                console.error('Error sending email:', error.message);
+            }
+            this.stop();
         },
-        completed: false
-    });
-
-    approachingDueTasks.forEach(async (task) => {
-        // Check if the reminder has already been sent
-        if (!task.reminderSent) {
-            const user = await userModel.findById(task.userId);
-            sendMail(user.email, "Task Due Soon", task.title, task.description);
-
-            // Update the task to indicate that a reminder has been sent
-            await taskModel.findByIdAndUpdate(task._id, { reminderSent: true });
-        }
-    });
+        { scheduled: true }
+    );
 };
 
-// Schedule the job to run every minute (adjust the cron schedule as needed)
-cron.schedule('* * * * *', () => {
-    console.log('Running task due time check job...');
-    checkDueTasks();
-});
-
 const addTask = async (req, res) => {
-    const { title, description, datetime } = req.body;
+    const { title, description, datetime, userTimeZone } = req.body;
     const userId = req.user.id;
-    const user = await userModel.findById(userId);
-    const newTask = new taskModel({ title, description, datetime, completed: false, userId });
+    const adjustedDateTime = moment(datetime).subtract(5, 'hours').subtract(30, 'minutes');
+    const utcDateTime = adjustedDateTime.utc().format();
 
-    newTask.save()
-        .then(() => {
-            // Send email notification for the added task
-            sendMail(user.email, "Task Added", title, description);
-            return res.status(200).json({ message: "Task added successfully" });
-        })
-        .catch((error) => {
-            return res.status(500).json({ message: error.message });
+    try {
+        const user = await userModel.find({ _id: userId });
+        if (!user || user.length === 0) {
+            console.error("User not found for ID:", userId);
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const newTask = new taskModel({
+            title,
+            description,
+            datetime: utcDateTime,
+            completed: false,
+            userId,
+            userTimeZone,
+            email: user[0].email,
         });
+
+        const savedTask = await newTask.save();
+        sendMail(user[0].email, "Task Added", title, description, false, false);
+        scheduleEmail(savedTask);
+
+        return res.status(200).json({ message: "Task added successfully" });
+    } catch (error) {
+        console.error("Error adding task:", error);
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 const removeTask = (req, res) => {
